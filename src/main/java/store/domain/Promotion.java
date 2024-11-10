fb97b31e91bdb1e301b88e6481f6fdbdc0ae5ed7
jher235 <tim668666@gmail.com>
2024-11-10 21:47:35 +0900

    feat: 날짜가 프로모션 기간에 포함되는지 확인

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,8 +1,16 @@
 package store.domain;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
 
 public record Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate){
 
+    public boolean isPromotionPeriod(){
+        return !startDate.isAfter(ChronoLocalDate.from(DateTimes.now())) &&
+                !endDate.isBefore(ChronoLocalDate.from(DateTimes.now()));
+    }
 
 }
